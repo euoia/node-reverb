9ce81418fc63f1499fce50de4ba053b773924400
James Pickard <james.pickard+github@gmail.com>
2014-03-31 13:06:37 -0400

    ensure to end the response

--- app/routes/prefs.js
+++ app/routes/prefs.js
@@ -4,10 +4,12 @@ var _ = require('underscore'),
 // Rather than store the selected verbs, we'll store the deselected one.
 exports.deselect = function (req, res) {
   prefs.deselectVerb(req.session, req.body.verb);
-  console.log('Deselected %s', verb);
+  console.log('Deselected %s', req.body.verb);
+  res.end();
 };
 
 exports.select = function (req, res) {
   prefs.selectVerb(req.session, req.body.verb);
-  console.log('Selected %s', verb);
+  console.log('Selected %s', req.body.verb);
+  res.end();
 };
